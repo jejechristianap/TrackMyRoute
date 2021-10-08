b1c6e3674b8f711afd5159c7057927b27ae90a13
jeremia <jeremia@cashtree.id>
2021-10-08 11:40:37 +0700

    AppModule dagger hilt

---
+++ app/src/main/java/com/example/trackmyroute/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.example.trackmyroute.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.trackmyroute.db.RunningDatabase
+import com.example.trackmyroute.other.Constant.RUNNING_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+/**
+ *   Created by Jeje on 10/8/21
+ **/
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+}
