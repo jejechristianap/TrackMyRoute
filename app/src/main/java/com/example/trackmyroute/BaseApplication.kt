d471b832c38db8cd779f5d4b28576505cf2f7203
jeremia <jeremia@cashtree.id>
2021-10-08 11:29:33 +0700

    BaseApplication setup

---
+++ app/src/main/java/com/example/trackmyroute/BaseApplication.kt
@@ -0,0 +1,12 @@
+package com.example.trackmyroute
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *   Created by Jeje on 10/8/21
+ **/
+
+@HiltAndroidApp
+class BaseApplication: Application {
+}
