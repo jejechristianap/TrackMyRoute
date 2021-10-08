e0a2e2bedbdf327424fd4b00a55d8ba7b4d0baa6
jeremia <jeremia@cashtree.id>
2021-10-08 13:00:08 +0700

    ViewModel setup

--- app/src/main/java/com/example/trackmyroute/ui/fragments/StatisticFragment.kt
+++ app/src/main/java/com/example/trackmyroute/ui/fragments/StatisticFragment.kt
@@ -1,10 +1,19 @@
 package com.example.trackmyroute.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.trackmyroute.R
+import com.example.trackmyroute.ui.viewmodels.MainViewModel
+import com.example.trackmyroute.ui.viewmodels.StatisticViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  *   Created by Jeje on 10/8/21
  **/
+
+@AndroidEntryPoint
 class StatisticFragment: Fragment(R.layout.fragment_statistic) {
+
+    private val viewModel: StatisticViewModel by viewModels()
+
 }
--- app/src/main/java/com/example/trackmyroute/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/trackmyroute/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,17 @@
 package com.example.trackmyroute.ui.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.trackmyroute.R
+import com.example.trackmyroute.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  *   Created by Jeje on 10/8/21
  **/
+
+@AndroidEntryPoint
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
+
+    private val viewModel: MainViewModel by viewModels()
 }
